d4c02d371874913fc2f050b295009e0ad49cbced
Ashish Bhandari <officialashish555@gmail.com>
2023-12-02 18:55:45 +0530

    first commit

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todo'
+
+function App() {
+
+
+  return (
+    <>
+     <h1>Learn redux toolkit</h1>
+     <Todos />
+     <AddTodo />
+    </>
+  )
+}
+
+export default App
